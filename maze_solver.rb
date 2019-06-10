aa3c3d90ef6b2490c240a627324a5026ba95995c
RT <rotyflo@gmail.com>
2019-06-10 09:27:17 -0700

    Refactor code to be more readable and modifiable from top

--- maze_solver.rb
+++ maze_solver.rb
@@ -1,12 +1,12 @@
 class MazeSolver
-  attr_writer :maze
-
   def initialize
     @file = ARGV[0]
     @maze = File.open(@file).map(&:chomp)
     @starting_point = nil
     @ending_point = nil
     @visited_points = []
+    @footprints = "."
+    @time_between_steps = 0.05
   end
 
   def solve
@@ -22,53 +22,56 @@ class MazeSolver
   end
 
   def wander
-    @visited_points.each do |point|
-      sleep(0.1)
+    @visited_points.each do |pos|
+      sleep(@time_between_steps)
       render
-      y, x = point
 
-      if up(point) == " "
-        point = [y - 1, x]
-        @visited_points << point
-        @maze[y - 1][x] = "X"
-      elsif right(point) == " "
-        point = [y, x + 1]
-        @visited_points << point
-        @maze[y][x + 1] = "X"
-      elsif down(point) == " "
-        point = [y + 1, x]
-        @visited_points << point
-        @maze[y + 1][x] = "X"
-      elsif left(point) == " "
-        point = [y, x - 1]
-        @visited_points << point
-        @maze[y][x - 1] = "X"
+      if can_walk_up_from?(*pos)
+        move_up_from(*pos)
+      elsif can_walk_right_from?(*pos)
+        move_right_from(*pos)
+      elsif can_walk_down_from?(*pos)
+        walk_down_from(*pos)
+      elsif can_walk_left_from?(*pos)
+        walk_left_from(*pos)
       end
     end
   end
 
-  def up(pos)
-    y, x = pos
-
-    @maze[y - 1][x]
+  def can_walk_up_from?(y, x)
+    @maze[y - 1][x] == " "
   end
 
-  def down(pos)
-    y, x = pos
-
-    @maze[y + 1][x]
+  def can_walk_down_from?(y, x)
+    @maze[y + 1][x] == " "
   end
 
-  def left(pos)
-    y, x = pos
-
-    @maze[y][x - 1]
+  def can_walk_left_from?(y, x)
+    @maze[y][x - 1] == " "
   end
 
-  def right(pos)
-    y, x = pos
+  def can_walk_right_from?(y, x)
+    @maze[y][x + 1] == " "
+  end
 
-    @maze[y][x + 1]
+  def move_up_from(y, x)
+    @visited_points << [y - 1, x]
+    @maze[y - 1][x] = @footprints
+  end
+
+  def move_right_from(y, x)
+    @visited_points << [y, x + 1]
+    @maze[y][x + 1] = @footprints
+  end
+
+  def walk_down_from(y, x)
+    @visited_points << [y + 1, x]
+    @maze[y + 1][x] = @footprints
+  end
+
+  def walk_left_from(y, x)
+    @visited_points << [y, x - 1]
+    @maze[y][x - 1] = @footprints
   end
 
   def render
@@ -79,13 +82,21 @@ class MazeSolver
   def solved?
     y, x = @ending_point
 
-    @maze[y + 1][x] == "X" || @maze[y - 1][x] == "X" || @maze[y][x + 1] == "X" || @maze[y][x - 1] == "X"
+    @maze[y + 1][x] == @footprints || @maze[y - 1][x] == @footprints || @maze[y][x + 1] == @footprints || @maze[y][x - 1] == @footprints
   end
 
   def find_start_and_end
     @maze.each_with_index do |string, index|
-      @starting_point = [index, string.index("S")] if string.index("S")
-      @ending_point = [index, string.index("E")] if string.index("E")
+      y = index
+
+      if string.index("S")
+        x = string.index("S")
+        @starting_point = [y, x]
+      end
+      if string.index("E")
+        x = string.index("E")
+        @ending_point = [y, x]
+      end
     end
   end
 
